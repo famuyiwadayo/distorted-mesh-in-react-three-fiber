c7bff6d62c73b413b098b106c88b615093ffefe7
Famuyiwa Dayo <famuyiwadayodaniel@gmail.com>
2020-11-19 15:37:58 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from 'three';
+import React, {useRef, useState, Suspense, useMemo} from 'react';
+import { Canvas, useFrame, useResource,  } from 'react-three-fiber';
+import { EffectComposer, DepthOfField, Bloom, Noise, Vignette } from 'react-postprocessing';
+import { Icosahedron, MeshDistortMaterial, useCubeTextureLoader, useTextureLoader, Html } from 'drei'
+
+function MainSphere({ material }: any) {
+  const main = useRef<any>()
+  // main sphere rotates following the mouse position
+  useFrame(({ clock, mouse }: any) => {
+    main.current.rotation.z = clock.getElapsedTime()
+    main.current.rotation.y = THREE.MathUtils.lerp(main.current.rotation.y, mouse.x * Math.PI, 0.1)
+    main.current.rotation.x = THREE.MathUtils.lerp(main.current.rotation.x, mouse.y * Math.PI, 0.1)
+  })
+  return <Icosahedron args={[1, 4]} ref={main} material={material} position={[0, 0, 0]} />
+}
+
+function Instances({ material }: any) {
+  // we use this array ref to store the spheres after creating them
+  const [sphereRefs] = useState<any>(() => [])
+  // we use this array to initialize the background spheres
+  const initialPositions = [
+    [-4, 20, -12],
+    [-10, 12, -4],
+    [-11, -12, -23],
+    [-16, -6, -10],
+    [12, -2, -3],
+    [13, 4, -12],
+    [14, -2, -23],
+    [8, 10, -20],
+  ]
+  // smaller spheres movement
+  useFrame(() => {
+    // animate each sphere in the array
+    sphereRefs.forEach((el: any) => {
+      el.position.y += 0.02
+      if (el.position.y > 19) el.position.y = -18
+      el.rotation.x += 0.06
+      el.rotation.y += 0.06
+      el.rotation.z += 0.02
+    })
+  })
+  return (
+    <>
+      <MainSphere material={material} />
+      {initialPositions.map((pos, i) => (
+        <Icosahedron
+          args={[1, 4]}
+          position={[pos[0], pos[1], pos[2]]}
+          material={material}
+          key={i}
+          ref={(ref) => (sphereRefs[i] = ref)}
+        />
+      ))}
+    </>
+  )
+}
+
+function Scene() {
+  const bumpMap = useTextureLoader("/bump.jpg")
+  const envMap = useCubeTextureLoader(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"], { path: "/cube/" })
+  // We use `useResource` to be able to delay rendering the spheres until the material is ready
+  const matRef = useResource<any>()
+
+  const material = useMemo(() => matRef.current, [matRef])
+
+  return (
+    <>
+      <MeshDistortMaterial
+        ref={matRef}
+        envMap={envMap}
+        bumpMap={bumpMap as THREE.Texture}
+        color={"#010101"}
+        roughness={0.1}
+        metalness={1}
+        bumpScale={0.005}
+        clearcoat={1}
+        clearcoatRoughness={1}
+        radius={1}
+        distort={0.4}
+      />
+      {matRef && <Instances material={matRef?.current} />}
+    </>
+  )
+}
 
 function App() {
+  const bgColor = new THREE.Color('#050505');
+  const color  = new THREE.Color("#161616")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      colorManagement
+      camera={{ position: [0, 0, 3] }}
+      gl={{ powerPreference: "high-performance", alpha: false, antialias: false, stencil: false, depth: false }}
+    >
+      <color attach="background" args={['#050505'] as any} />
+      <fog color={color} attach="fog" near={8} far={30} />
+
+      <Suspense fallback={<Html center>Loading.</Html>}>
+        <Scene />
+      </Suspense>
+
+      <EffectComposer>
+        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} opacity={3} />
+        <Noise opacity={0.025} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      </EffectComposer>
+    </Canvas>
   );
 }
 
